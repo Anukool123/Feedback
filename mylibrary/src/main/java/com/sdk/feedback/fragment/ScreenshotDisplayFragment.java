7fad58347ac03d20c5e4590d4c75abb7520ab9f5
310124463 <anukool.tiwari@philips.com>
2016-04-11 14:03:14 +0200

    Change with respect to the screenshot.

--- mylibrary/src/main/java/com/sdk/feedback/fragment/ScreenshotDisplayFragment.java
+++ mylibrary/src/main/java/com/sdk/feedback/fragment/ScreenshotDisplayFragment.java
@@ -6,7 +6,9 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import com.sdk.feedback.R;
 import com.sdk.feedback.Screenshot;
@@ -18,17 +20,26 @@ import com.sdk.feedback.util.AppConstants;
  */
 public class ScreenshotDisplayFragment extends BaseFragment {
 
-    public static final String TAG =Screenshot.class.getSimpleName() ;
+    public static final String TAG = Screenshot.class.getSimpleName();
 
-    private String imagePath ;
+    private String imagePath;
 
-    private ImageView ivScreenShot ;
+    private ImageView ivScreenShot;
+
+    private Button btnOk;
+
+    private RelativeLayout rlInstructionToSave;
+
+    /*
+    1. Draw on screen or tap save
+    2. Save and cancel button
+     */
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.com_sdk_feedback_screenshot_fragment,container,false);
+        View view = inflater.inflate(R.layout.com_sdk_feedback_screenshot_fragment, container, false);
 
         return view;
     }
@@ -40,14 +51,27 @@ public class ScreenshotDisplayFragment extends BaseFragment {
 
         Bundle bundle = getArguments();
 
-        if(bundle != null)
-        {
-            imagePath = bundle.getString(AppConstants.IMAGE_PATH , null);
+        if (bundle != null) {
+            imagePath = bundle.getString(AppConstants.IMAGE_PATH, null);
         }
 
         ivScreenShot = (ImageView) getView().findViewById(R.id.ivScreenshotImage);
 
-        ivScreenShot.setImageBitmap(BitmapFactory.decodeFile(imagePath,null));
+        ivScreenShot.setImageBitmap(BitmapFactory.decodeFile(imagePath, null));
+
+        rlInstructionToSave = (RelativeLayout) getView().findViewById(R.id.rlInstructionToSave);
+
+        btnOk = (Button) getView().findViewById(R.id.btnOk);
+
+        btnOk.setOnClickListener(new View.OnClickListener() {
+                                     @Override
+                                     public void onClick(View view) {
+                                         if (rlInstructionToSave != null && rlInstructionToSave.isShown()) {
+                                             rlInstructionToSave.setVisibility(View.GONE);
+                                         }
+                                     }
+                                 }
+        );
 
 
     }
