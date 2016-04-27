072bc71e6f354e1a5ab3263a226d7f0d86903a88
310124463 <anukool.tiwari@philips.com>
2016-04-27 11:26:46 +0200

    Workaround for texture views

--- mylibrary/src/main/java/com/sdk/feedback/Screenshot.java
+++ mylibrary/src/main/java/com/sdk/feedback/Screenshot.java
@@ -3,13 +3,21 @@ package com.sdk.feedback;
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.TextureView;
 import android.view.View;
+import android.view.ViewGroup;
 
 import com.sdk.feedback.util.AppConstants;
 import com.sdk.feedback.util.AppUtility;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by 310124463 on 4/11/2016.
@@ -17,10 +25,13 @@ import java.io.FileOutputStream;
 public class Screenshot {
 
 
+    private static final String TAG = Screenshot.class.getSimpleName() ;
+
     public static String takeScreenShot(Context context) {
 
         Bitmap screenCaptured = null;
         try {
+
             // image naming and path  to include sd card  appending name you choose for file
             String mPath = AppUtility.getCapturedScreenShotPath(context);
 
@@ -30,10 +41,47 @@ public class Screenshot {
             System.out.println("Screenshot.takeScreenShot:"+ mPath);
 
             // create bitmap screen capture
-            View v1 =((Activity)(context)). getWindow().getDecorView().getRootView();
-            v1.setDrawingCacheEnabled(true);
-            screenCaptured = Bitmap.createBitmap(v1.getDrawingCache());
-            v1.setDrawingCacheEnabled(false);
+            View rootView =((Activity)(context)). getWindow().getDecorView().getRootView();
+            rootView.setDrawingCacheEnabled(true);
+            screenCaptured = Bitmap.createBitmap(rootView.getDrawingCache());
+          //  rootView.setDrawingCacheEnabled(false);
+
+            List<TextureView> textureViews = getAllTextureViews(rootView);
+
+            Log.i(TAG, "" + textureViews.size());
+
+            Canvas canvas = new Canvas(screenCaptured);
+
+            if(textureViews.size()>0)
+            {
+
+
+                for(TextureView textureView : textureViews)
+                {
+                    Bitmap bitmapForTextureView = textureView.getBitmap(textureView.getWidth(),textureView.getHeight());
+                    int[] location = new int[2];
+                    textureView.getLocationInWindow(location);
+                    int[] location2 = new int[2];
+                    textureView.getLocationOnScreen(location2);
+                    canvas.drawBitmap(bitmapForTextureView, location[0], location[1], null);
+
+                }
+            }
+
+            List<SurfaceView> surfaceViews = getAllSurfaceViews(rootView);
+            Log.i(TAG, "" + surfaceViews.size());
+
+            for(SurfaceView surfaceView : surfaceViews)
+            {
+                Bitmap bitmapForTextureView = getBitmap(surfaceView,surfaceView.getWidth(), surfaceView.getHeight());
+                int[] location = new int[2];
+                surfaceView.getLocationInWindow(location);
+                int[] location2 = new int[2];
+                surfaceView.getLocationOnScreen(location2);
+                canvas.drawBitmap(bitmapForTextureView, location[0], location[1], null);
+
+            }
+
 
             //Crop Status bar if visible
             boolean isFullScreen = AppUtility.isFullScreenApp(context);
@@ -66,6 +114,57 @@ public class Screenshot {
         }
 
 
+    }
+
+    private static Bitmap getBitmap(SurfaceView surfaceView, int width, int height) {
+
+        surfaceView.getDrawingCache();
+
+        SurfaceHolder holder = surfaceView.getHolder();
+
+        Bitmap.Config conf = Bitmap.Config.RGB_565;
+
+        Bitmap image = Bitmap.createBitmap(width , height, conf);
+
+        Canvas canvas  = holder.lockCanvas(null);
+
+        canvas.setBitmap(image);
+
+        return null;
+    }
+
+
+    public static  List<TextureView> getAllTextureViews(View view)
+    {
+        List<TextureView> textureViews = new ArrayList<TextureView>();
+        if (view instanceof TextureView) {
+            textureViews.add((TextureView) view);
+        }
+        else if(view instanceof ViewGroup)
+        {
+            ViewGroup viewGroup = (ViewGroup)view;
+            for (int i = 0; i < viewGroup.getChildCount(); i++) {
+                textureViews.addAll(getAllTextureViews(viewGroup.getChildAt(i)));
+            }
+        }
+        return textureViews;
+    }
+
+
+    public static  List<SurfaceView> getAllSurfaceViews(View view)
+    {
+        List<SurfaceView> surfaceViews = new ArrayList<SurfaceView>();
+        if (view instanceof SurfaceView) {
+            surfaceViews.add((SurfaceView)view);
+        }
+        else if(view instanceof ViewGroup)
+        {
+            ViewGroup viewGroup = (ViewGroup)view;
+            for (int i = 0; i < viewGroup.getChildCount(); i++) {
+                surfaceViews.addAll(getAllSurfaceViews(viewGroup.getChildAt(i)));
+            }
+        }
+        return surfaceViews;
     }
 
 //    private static int getStatusBarHeight(Context context)
